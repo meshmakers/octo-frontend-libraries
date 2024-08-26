f76463c9178409b40206356681ce2f5b14583d7c
Gerald Lochner <gerald.lochner@meshmakers.io>
2024-08-26 17:36:30 +0200

    AB#1102: New: Remove adapters for pools

--- src/FrontendLibraries/ClientApp/projects/meshmakers/shared-services/src/lib/dataSources/dataSourceBase.ts
+++ src/FrontendLibraries/ClientApp/projects/meshmakers/shared-services/src/lib/dataSources/dataSourceBase.ts
@@ -11,21 +11,50 @@ export class DataSourceBase<TDto> implements DataSource<TDto> {
   public readonly totalCount$ = this.totalCountSubject.asObservable();
 
   private readonly addedDtoList: TDto[] = new Array<TDto>();
+  private readonly removedDtoList: TDto[] = new Array<TDto>();
   private lastPagedResult: PagedResultDto<TDto> | null = null;
 
   public get totalCount(): Observable<number> {
     return this.totalCount$;
   }
 
+  public reset(): void {
+    this.addedDtoList.length = 0;
+    this.removedDtoList.length = 0;
+  }
+
   public addTemporaryDto(dto: TDto): void {
 
-    this.addedDtoList.push(dto);
+    const index = this.removedDtoList.indexOf(dto);
+    if (index !== -1) {
+      this.removedDtoList.splice(index, 1);
+    } else {
+      this.addedDtoList.push(dto);
+    }
+
     this.onEndLoad();
   }
 
-  public get temporaryDtos(): TDto[] {
+  public removeTemporaryDto(dto: TDto): void {
+
+    const index = this.addedDtoList.indexOf(dto);
+    if (index !== -1) {
+      this.addedDtoList.splice(index, 1);
+    } else {
+      this.removedDtoList.push(dto);
+    }
+
+    this.onEndLoad();
+  }
+
+  public get addedTemporaryDtos(): TDto[] {
     return this.addedDtoList;
   }
+
+  public get removedTemporaryDtos(): TDto[] {
+    return this.removedDtoList;
+  }
+
 
   connect(collectionViewer: CollectionViewer): Observable<readonly TDto[]> {
     return this.dataSubject.asObservable();
@@ -56,8 +85,8 @@ export class DataSourceBase<TDto> implements DataSource<TDto> {
 
     if (this.lastPagedResult !== null) {
 
-      const totalCount = this.lastPagedResult?.totalCount + this.addedDtoList.length;
-      const list = this.lastPagedResult.list.concat(this.addedDtoList);
+      const totalCount = this.lastPagedResult?.totalCount + this.addedDtoList.length - this.removedDtoList.length;
+      const list = this.lastPagedResult.list.concat(this.addedDtoList).filter((x) => !this.removedDtoList.includes(x));
 
       this.loadingSubject?.next(false);
       this.dataSubject?.next(list);
