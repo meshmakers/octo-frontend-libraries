c088dec487166a3095980a2123c0d30617d453da
Lukas Stoiber <lukas.stoiber@gmx.net>
2024-09-16 09:03:17 +0200

    import octo-ui works (#2)

--- src/FrontendLibraries/ClientApp/projects/meshmakers/octo-ui/src/public-api.ts
+++ src/FrontendLibraries/ClientApp/projects/meshmakers/octo-ui/src/public-api.ts
@@ -8,3 +8,4 @@ export * from './lib/list-element/newGraphQlDataSource';
 export * from './lib/list-element/globalTypes';
 
 export * from './lib/mm-octo-table/mm-octo-table.component';
+export * from './lib/mm-octo-ui.module';
