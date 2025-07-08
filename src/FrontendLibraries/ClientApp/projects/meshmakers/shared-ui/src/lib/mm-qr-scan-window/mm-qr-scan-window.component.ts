523b691c0b2961f96f803397362bd709ddbe380d
PhilippSchwaab <philipp.schwaab@meshmakers.io>
2025-07-08 15:49:53 +0200

    AB#2307: Add Camera permission check and error snackbars

--- src/FrontendLibraries/ClientApp/projects/meshmakers/shared-ui/src/lib/mm-qr-scan-window/mm-qr-scan-window.component.ts
+++ src/FrontendLibraries/ClientApp/projects/meshmakers/shared-ui/src/lib/mm-qr-scan-window/mm-qr-scan-window.component.ts
@@ -1,20 +1,36 @@
-﻿import { Component, ElementRef, ViewChild, OnDestroy } from "@angular/core";
+﻿import { Component, ElementRef, ViewChild, OnDestroy, OnInit } from "@angular/core";
 import { MatDialog, MatDialogRef } from "@angular/material/dialog";
 import { QrCodeScannerService } from '@meshmakers/shared-services';
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { firstValueFrom } from "rxjs";
 
 @Component({
   selector: 'mm-qr-code-scanner',
   templateUrl: './mm-qr-scan-window.component.html',
   styleUrl: './mm-qr-scan-window.component.css'
 })
-export class MmQrCodeScannerComponent implements OnDestroy {
+export class MmQrCodeScannerComponent implements OnInit, OnDestroy {
   @ViewChild('video', { static: true }) videoRef!: ElementRef<HTMLVideoElement>;
 
   scanning = false;
   constructor(
     private dialogRef: MatDialogRef<MmQrCodeScannerComponent>,
+    private snackBar: MatSnackBar,
     private scannerService: QrCodeScannerService
   ) {}
+
+  private async checkCameraPermission(): Promise<boolean> {
+    if (!navigator.permissions || !navigator.mediaDevices) return true;
+
+    try {
+      const status = await navigator.permissions.query({ name: 'camera' as PermissionName });
+      return status.state !== 'denied';
+
+    } catch {
+      return true; // fallback for Safari or unsupported browsers
+    }
+  }
+
 
   static open(dialog: MatDialog): Promise<string | null> {
     const ref = dialog.open(MmQrCodeScannerComponent, {
@@ -24,13 +40,38 @@ export class MmQrCodeScannerComponent implements OnDestroy {
       disableClose: true
     });
 
-    return ref.afterClosed().toPromise();
+    return firstValueFrom(ref.afterClosed());
   }
 
   async ngOnInit() {
+
+    const hasPermission = await this.checkCameraPermission();
+
+    if (!hasPermission) {
+      this.snackBar.open(
+        'Camera access is blocked. Please enable it in your browser settings.',
+        'OK',
+        {
+          duration: 5000,
+          horizontalPosition: 'center',
+          verticalPosition: 'top',
+        }
+      );
+      this.dialogRef.close(null);
+      return;
+    }
+
     const supported = await this.scannerService.isSupported();
     if (!supported) {
-      alert("QR scanning is not supported in this browser.");
+      this.snackBar.open(
+        'QR scanning is not supported in this browser.',
+        'OK',
+        {
+          duration: 5000,
+          horizontalPosition: 'center',
+          verticalPosition: 'top',
+        }
+        );
       this.dialogRef.close(null);
       return;
     }
